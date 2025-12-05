6753c2e986cae6c8cd9ba64b4b477a37544fe39d
satakshi003 <soumya3955@gmail.com>
2025-12-05 23:29:28 +0530

    refactor: clean up user registration controller by removing unnecessary whitespace

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,8 @@ import {User} from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
+
+
 const registerUser = asyncHandler(async (req, res) => {
 //get user details from frontend
 //validation - not empty
@@ -14,6 +16,8 @@ const registerUser = asyncHandler(async (req, res) => {
 //remove password and refresh token field from response
 //check for user creation
 //send success response 
+
+
 
 const {fullName, username, email, password} = req.body;
 console.log("email:", email);
@@ -35,12 +39,19 @@ if(existedUser){
 const avatarLocalPath = req.files?.avatar[0]?.path;
 const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
+
+
 if(!avatarLocalPath){
   throw new ApiError(400, "Avatar is required");
 }
 
 const avatar = await uploadOnCloudinary(avatarLocalPath);
+
+
+
+
 const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
 if(!avatar){
   throw new ApiError(500, "Could not upload avatar. Please try again"); 
 }
