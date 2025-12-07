326bd4421a68c619451e1ca25e78be9ac2ad7313
satakshi003 <soumya3955@gmail.com>
2025-12-07 14:43:06 +0530

    feat: implement JWT verification middleware for user authentication

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,30 @@
+import { ApiError } from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.model.js";
+
+export const verifyJWT = asyncHandler (async (req, res, next) => {
+  //get token from headers
+  //if not present, throw error 
+  try {
+    const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "") 
+    if(!token)
+    {
+      throw new ApiError(401, "Unauthorized access, token missing")
+    }
+    
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+  
+    const user = await User.findById (decodedToken?._id).select("-password -refreshToken")
+    if(!user)
+    {
+      throw new ApiError (401, "Unauthorized access, user not found")
+    }
+  
+    req.user = user
+    next()
+  } catch (error) {
+    throw new ApiError (401, "Unauthorized access, invalid token")
+  }
+});
+
