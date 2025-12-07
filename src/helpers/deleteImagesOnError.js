f85db7ef912bd6144b1bce2bf047dac5f920bc5b
satakshi003 <soumya3955@gmail.com>
2025-12-07 14:42:36 +0530

    feat: add helper functions for image deletion, token generation, and email validation

---
+++ src/helpers/deleteImagesOnError.js
@@ -0,0 +1,13 @@
+
+function deleteImagesOnError(req){
+  const avatarLocalPath = req.files?.avatar
+      ? req.files.avatar[0]?.path
+      : null;
+    const coverImageLocalPath = req.files?.coverImage
+      ? req.files.coverImage[0]?.path
+      : null;
+    if(avatarLocalPath) fs.unlinkSync(avatarLocalPath);
+    if(coverImageLocalPath) fs.unlinkSync(coverImageLocalPath);
+}
+
+export default deleteImagesOnError;
---
+++ src/helpers/validateEmail.js
@@ -0,0 +1,6 @@
+function validateEmail(email) {
+  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+  return emailRegex.test(email);
+}  
+
+export default validateEmail;
