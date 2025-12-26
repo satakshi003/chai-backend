129cd2ab30d0ee53c91fab9d80ae5245d4c04816
satakshi003 <soumya3955@gmail.com>
2025-12-26 14:08:53 +0530

    feat: implement tweet management with create and fetch functionality

---
+++ src/controllers/tweet.controller.js
@@ -0,0 +1,49 @@
+import mongoose, { isValidObjectId } from "mongoose"
+import {Tweet} from "../models/tweet.model.js"
+import {User} from "../models/user.model.js"
+import {ApiError} from "../utils/ApiError.js"
+import {ApiResponse} from "../utils/ApiResponse.js"
+import {asyncHandler} from "../utils/asyncHandler.js"
+
+const createTweet = asyncHandler(async (req, res) => {
+    //TODO: create tweet
+ const {content} = req.body;
+ 
+ if(!content|| content.trim().length === 0){
+  throw new ApiError(400,"Tweet content is required");
+ }
+ const tweet = await Tweet.create({
+  content,
+  owner: req.user._id
+ });
+
+ return res 
+ .status(201)
+ .json(new ApiResponse(201, tweet, "Tweet created successfully"))
+});
+
+const getUserTweets = asyncHandler(async (req, res) => {
+    // TODO: get user tweets
+  const userId = req.user._id;
+
+  const tweets = await Tweet.find({owner: userId}).sort({createdAt: -1});
+
+  return res
+  .status(200)
+  .json(new ApiResponse(200, tweets, "User tweets fetched successfully" ));
+});
+
+const updateTweet = asyncHandler(async (req, res) => {
+    //TODO: update tweet
+})
+
+const deleteTweet = asyncHandler(async (req, res) => {
+    //TODO: delete tweet
+})
+
+export {
+    createTweet,
+    getUserTweets,
+    updateTweet,
+    deleteTweet
+}
