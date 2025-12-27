a8c6604363c999fba770ba0ff55c4e96116cd0b0
satakshi003 <soumya3955@gmail.com>
2025-12-27 19:55:11 +0530

    feat: implement delete functionality for comments with validation and ownership checks

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -98,8 +98,27 @@ const updateComment = asyncHandler(async (req, res) => {
 
 const deleteComment = asyncHandler(async (req, res) => {
     // TODO: delete a comment
-    
-})
+  const {commentId} = req.params
+  
+  if(!isValidObjectId(commentId)){
+    throw new ApiError(400, "Invalid comment ID");
+  }
+
+  const comment = await Comment.findById(commentId);
+  if(!comment){
+     throw new ApiError(404, "Comment not found");
+  }
+
+  if(comment.owner.toString() !== req.user._id.toString()){
+      throw new ApiError(403, "You are not allowed to delete this comment");
+    }
+
+  await Comment.findByIdAndDelete(commentId);
+
+  return res
+    .status(200)
+        .json(new ApiResponse(200, {}, "Comment deleted successfully"));
+});
 
 export {
     getVideoComments, 
