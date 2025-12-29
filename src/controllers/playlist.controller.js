b3c4bc52421e7547e7be33269c9e3d9f25688150
satakshi003 <soumya3955@gmail.com>
2025-12-29 19:35:19 +0530

    feat: implement playlist controller with create, get, update, delete, and video management functionalities

---
+++ src/controllers/playlist.controller.js
@@ -0,0 +1,74 @@
+import mongoose, {isValidObjectId} from "mongoose"
+import {Playlist} from "../models/playlist.model.js"
+import {ApiError} from "../utils/ApiError.js"
+import {ApiResponse} from "../utils/ApiResponse.js"
+import {asyncHandler} from "../utils/asyncHandler.js"
+
+
+const createPlaylist = asyncHandler(async (req, res) => {
+  //TODO: create playlist
+    const {name, description} = req.body
+    
+    if(!name || name.trim().length === 0){
+      throw new ApiError(400, "Playlist name is required");
+    }
+
+    const playlist = await Playlist.create({
+      name,
+      description,
+      owner: req.user._id,
+      videos: [] // newly created playlist starts empty
+    });
+
+    return res
+    .status(201)
+    .json(
+      new ApiResponse(201, playlist, "Playlist created successfully")
+    );
+    
+})
+
+const getUserPlaylists = asyncHandler(async (req, res) => {
+    const {userId} = req.params
+    //TODO: get user playlists
+
+    if(!isValidObjectId(userId)){
+      
+    }
+})
+
+const getPlaylistById = asyncHandler(async (req, res) => {
+    const {playlistId} = req.params
+    //TODO: get playlist by id
+})
+
+const addVideoToPlaylist = asyncHandler(async (req, res) => {
+    const {playlistId, videoId} = req.params
+})
+
+const removeVideoFromPlaylist = asyncHandler(async (req, res) => {
+    const {playlistId, videoId} = req.params
+    // TODO: remove video from playlist
+
+})
+
+const deletePlaylist = asyncHandler(async (req, res) => {
+    const {playlistId} = req.params
+    // TODO: delete playlist
+})
+
+const updatePlaylist = asyncHandler(async (req, res) => {
+    const {playlistId} = req.params
+    const {name, description} = req.body
+    //TODO: update playlist
+})
+
+export {
+    createPlaylist,
+    getUserPlaylists,
+    getPlaylistById,
+    addVideoToPlaylist,
+    removeVideoFromPlaylist,
+    deletePlaylist,
+    updatePlaylist
+}
