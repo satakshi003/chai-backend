b3d006dfb5f696bcc3a17a77c269b18b2f8d0c5d
satakshi003 <soumya3955@gmail.com>
2025-12-30 14:47:58 +0530

    feat: implement playlist routes with JWT verification for playlist management

---
+++ src/routes/playlist.routes.js
@@ -0,0 +1,36 @@
+import { Router } from 'express';
+import {
+    addVideoToPlaylist,
+    createPlaylist,
+    deletePlaylist,
+    getPlaylistById,
+    getUserPlaylists,
+    removeVideoFromPlaylist,
+    updatePlaylist,
+} from "../controllers/playlist.controller.js"
+import {verifyJWT} from "../middlewares/auth.middleware.js"
+
+const router = Router();
+
+router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
+
+router.route("/").post(createPlaylist)
+
+router.route("/user/:userId").get(getUserPlaylists);
+
+router
+    .route("/:playlistId")
+    .get(getPlaylistById)
+    .patch(updatePlaylist)
+    .delete(deletePlaylist);
+
+router
+    .route("/:playlistId/videos/:videoId")
+    .patch(addVideoToPlaylist);
+
+router
+    .route("/:playlistId/videos/:videoId")
+    .delete(removeVideoFromPlaylist);
+
+
+export default router
