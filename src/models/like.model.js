177819163f51628f4d7325e7046c5d166caa0627
satakshi003 <soumya3955@gmail.com>
2025-12-31 15:09:10 +0530

    fix: correct casing in like model field names for consistency

--- src/models/like.model.js
+++ src/models/like.model.js
@@ -5,7 +5,7 @@ const likeSchema = new Schema({
     type: Schema.Types.ObjectId,
     ref: "Video"
   },
-   Comment: {
+   comment: {
     type: Schema.Types.ObjectId,
     ref: "Comment"
   },
@@ -13,7 +13,7 @@ const likeSchema = new Schema({
     type: Schema.Types.ObjectId,
     ref: "Tweet"
   },
-   likedby: {
+   likedBy: {
     type: Schema.Types.ObjectId,
     ref: "User"
   }
